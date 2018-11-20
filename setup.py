c58bddd14f510b15e069020c15d1c083e6e0dd76
rcbensley <richardbensley@gmail.com>
2018-11-20 19:21:51 +0000

    cleanup, using prettytable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='mqm',
+      version='1.0',
+      description='Multi-Query-MariaDB',
+      author='Richard Bensley',
+      author_email='richardbensley@gmail.com',
+      url='https://github.com/rcbensley/mqm',
+      scripts=['./mqm'])
+
